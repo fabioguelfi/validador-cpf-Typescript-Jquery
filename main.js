4a1b8e27bcfbaf960a2c2d702a7a510dfec887ed
fabioguelfi <fabioguelfiunix@gmail.com>
2017-12-11 00:40:40 -0200

    entradaMinima()

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 "use strict";
 $(function () {
     var CalculosBasicos = /** @class */ (function () {
-        function CalculosBasicos(cpf) {
+        function CalculosBasicos(cpf, entrada) {
             this.cpf = parseInt(cpf);
+            this.entrada = entrada;
         }
         CalculosBasicos.prototype.getStatus = function () {
             return this.status;
+        };
+        CalculosBasicos.prototype.getAprovadaRecusada = function () {
+            return this.aprovadaRecusada;
         };
         CalculosBasicos.prototype.statusCpf = function () {
             var cpf = this.cpf.toString().length;
@@ -16,21 +20,36 @@ $(function () {
                 this.status = 'valido';
             }
         };
+        CalculosBasicos.prototype.entradaMinima = function () {
+            var entrada = this.entrada;
+            if (entrada > 5000) {
+                this.aprovadaRecusada = 'Aprovada';
+            }
+            else {
+                this.aprovadaRecusada = 'Recusada';
+            }
+        };
         return CalculosBasicos;
     }());
-    $('#cpf , #nome').bind('input', function () {
+    $('input').bind('input', function () {
         /* --- get values --- */
         var nome = $('#nome').val();
         var cpf = $('#cpf').val();
+        var entradafirst = $('#entrada').val();
+        var entrada = parseInt(entradafirst);
         /* --- metodo validacao --- */
-        var validador = new CalculosBasicos(cpf);
+        var validador = new CalculosBasicos(cpf, entrada);
         validador.statusCpf();
         var status = validador.getStatus();
+        validador.entradaMinima();
         /* --- renderiza no html --- */
         $('#nomeUser').html(nome);
         $('#nCpf').html(cpf);
         $('#status').html(status);
+        $('#valor').html(entrada);
+        $('#aprovadaRecusada').html(validador.getAprovadaRecusada());
         /* --- default css --- */
         $('span').css('color', 'red');
+        /* --- teste --- */
     });
 });
--- main.ts
+++ main.ts
@@ -4,12 +4,20 @@ $(function () {
 
         private cpf:any
         private status:string 
+        private entrada: number
+        private aprovadaRecusada: string
         
-        constructor(cpf:any) {
+        constructor(cpf:any, entrada:number) {
             this.cpf = parseInt(cpf)
+            this.entrada = entrada
         }
+
         public getStatus(){
             return this.status
+        }
+
+        public getAprovadaRecusada(){
+            return this.aprovadaRecusada
         }
 
         public statusCpf(){
@@ -20,28 +28,42 @@ $(function () {
                 this.status = 'valido'
             }
         }
+        public entradaMinima(){
+            let entrada: number = this.entrada
+            if(entrada > 5000){
+                this.aprovadaRecusada = 'Aprovada'
+            }else{
+                this.aprovadaRecusada = 'Recusada'
+            }
+        }
+
     }
     
 
-    $('#cpf , #nome').bind('input',function () {
+    $('input').bind('input',function () {
         
         /* --- get values --- */
         let nome: string = $('#nome').val()
         let cpf: any = $('#cpf').val()
-
+        let entradafirst: string = $('#entrada').val()
+        let entrada : number = parseInt(entradafirst)
         /* --- metodo validacao --- */
 
-        let validador = new CalculosBasicos(cpf)
+        let validador = new CalculosBasicos(cpf,entrada)
         validador.statusCpf()
         let status = validador.getStatus()
+        validador.entradaMinima()
 
         /* --- renderiza no html --- */
         $('#nomeUser').html(nome)
         $('#nCpf').html(cpf)
         $('#status').html(status)
-
+        $('#valor').html(entrada)
+        $('#aprovadaRecusada').html(validador.getAprovadaRecusada())
         /* --- default css --- */
         $('span').css('color','red')
+        
+        /* --- teste --- */
     })
    
 })
